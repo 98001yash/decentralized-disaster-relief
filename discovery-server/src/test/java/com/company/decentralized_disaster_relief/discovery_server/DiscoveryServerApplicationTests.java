29cf4ef6ae479759bc3f6f23100f308b8b97ea2c
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-02 09:33:34 +0530

    added the discovery-server

---
+++ discovery-server/src/test/java/com/company/decentralized_disaster_relief/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.decentralized_disaster_relief.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
