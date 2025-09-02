aabe1270ff4f8280ec2862ed4855392f4076c8e6
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-02 14:53:48 +0530

    added the org-service

---
+++ org-service/src/test/java/com/company/decentralized_disaster_relief/org_service/OrgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.decentralized_disaster_relief.org_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
