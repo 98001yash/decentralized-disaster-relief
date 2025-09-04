5e9870644f353a82d7d4fe4fcf77c65b941f47be
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-09-04 10:18:32 +0530

    added the authFlags to the auth-service also

---
+++ auth-service/src/main/java/com/company/decentralized_disaster_relief/auth_service/auth/AuthFlags.java
@@ -0,0 +1,23 @@
+package com.company.decentralized_disaster_relief.auth_service.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public final class AuthFlags {
+
+    public static Long requireUserId() {
+        Long id = UserContextHolder.getCurrentUserId();
+        if (id == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+        return id;
+    }
+
+    public static boolean isPlatformAdmin() {
+        return UserContextHolder.hasRole("ROLE_ADMIN");
+    }
+
+    public static boolean isPlatformVerifier() {
+        return UserContextHolder.hasRole("ROLE_VERIFIER");
+    }
+}
